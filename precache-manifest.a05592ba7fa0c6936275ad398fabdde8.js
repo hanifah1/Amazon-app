cdd1e3924fbcc5eb7533462a2fe2cdb469d9e332
hanifah <khanhanifah@gmail.com>
2019-02-27 10:39:00 -0500

    Updates

---
+++ precache-manifest.a05592ba7fa0c6936275ad398fabdde8.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f2b977a8773a4a6ab9ed02f012d64b0",
+    "url": "/Amazon-app/static/media/amazonCard.9f2b977a.png"
+  },
+  {
+    "revision": "895be2daebf61113e1fe",
+    "url": "/Amazon-app/static/css/main.636fd790.chunk.css"
+  },
+  {
+    "revision": "eb912e5c60697f32c4f2",
+    "url": "/Amazon-app/static/js/runtime~main.eb912e5c.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Amazon-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "3e9da4eba801a1dd4399",
+    "url": "/Amazon-app/static/js/2.3e9da4eb.chunk.js"
+  },
+  {
+    "revision": "9f5aac29df4acb3b2df32db1002774d7",
+    "url": "/Amazon-app/static/media/banner1.9f5aac29.jpg"
+  },
+  {
+    "revision": "895be2daebf61113e1fe",
+    "url": "/Amazon-app/static/js/main.895be2da.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Amazon-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Amazon-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Amazon-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Amazon-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "3e9da4eba801a1dd4399",
+    "url": "/Amazon-app/static/css/2.f43468e1.chunk.css"
+  },
+  {
+    "revision": "6357390caa529eb5a6e727708ac30924",
+    "url": "/Amazon-app/index.html"
+  }
+];
